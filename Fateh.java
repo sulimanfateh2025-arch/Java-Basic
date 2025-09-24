a0a167c5c80f560ddff4d9306a58cf0d670039fc
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-09-24 01:42:09 -0700

    Add files via upload

---
+++ Fateh.java
@@ -0,0 +1,20 @@
+
+package fateh;
+
+
+public class Fateh {
+
+    
+    public static void main(String[] args) {
+        String[] weekday = {"sat" , "sun" , "mon" , "tue" , "wen" , "Thu" , "fri"};
+        int x = 0;
+        while (x < 7){
+            System.out.println(weekday[x]);
+            x++ ;
+        
+        
+            
+      
+    
+    
+}
