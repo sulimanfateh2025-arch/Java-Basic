26470dbf39ce25bd77a98bdcc33fbbeeeb4c03ae
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-09-27 01:28:34 -0700

    Add files via upload

---
+++ Noorahmad.java
@@ -0,0 +1,57 @@
+
+package noorahmad;
+import java.util. Scanner;
+
+
+public class Noorahmad {
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Floating Number : ");
+         float num1 = sc.nextFloat();
+         
+        System.out.println("Enter Second Floating Number : ");
+         float num2 = sc.nextFloat();
+         
+         System.out.println("Enter third Floating Number : ");
+         float num3 = sc.nextFloat();
+         
+         float sum = num1 + num2 +num3;
+         System.out.println("the sum of the three floating numbers is: "+ sum);
+      /* int num1 = 20;
+       int num2 = 10;
+       
+       int differance = num1 - num2;
+       float num3 = 5f;
+       float multiply = differance * num3;
+       System.out.println(multiply);*/
+      
+//        System.out.println(" Enter any number in Kilometers :");
+//        float kilometers = sc.nextFloat();
+//        
+//        float meters = kilometers * 1000;
+//        System.out.println(kilometers+ "is" + meters);
+//        
+       /* System.out.println("Enter the Linght of  the rectrangle :");
+        int linght = sc.nextInt();
+        
+        System.out.println("Enter the size of the wide of the rectrangle:");
+        int wide = sc.nextInt();
+        
+        float area = linght * wide;
+        System.out.println("Area of the rectranngle is : " + area);*/
+       
+       /* System.out.println("Enter any number: ");
+        boolean num = sc.hasNextInt();
+        System.out.println(num);*/
+       
+       /* System.out.println("Enter your name : ");
+        String name = sc.next();
+        System.out.println( "hello " + name + "have a good day ");*/
+       
+
+ 
+    }
+    
+}
