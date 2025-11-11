e0d7c9585695c47664f4130201b1d0e421a6f2d1
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-11-11 20:41:15 +0430

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,28 @@
+
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args) {
+       int num1 = 120;
+       int num2 = 40;
+    
+      int result = num1 / num2;
+      System.out.println(result);
+        
+        
+       int a = 5;
+      System.out.println(a);
+       a--;
+       System.out.println(a);
+
+//int a = 10;
+a +=20;
+System.out.println(a);
+
+    System.out.println(55 == 55 || 55 == 100);
+       
+
+    }
+    
+}
