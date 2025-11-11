2714f02e520dd8bbda809c232120bfc4f44a8459
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-11-11 20:44:04 +0430

    Add files via upload

---
+++ Noori.java
@@ -0,0 +1,25 @@
+
+package noori;
+
+
+public class Noori {
+
+   
+    public static void main(String[] args) {
+        String[] weekday = {"sat" , "sun" , "mon" , "tue" , "wen" , "Thu" , "fri"};
+        System.out.println(weekday[0]);
+        System.out.println(weekday[1]);
+        System.out.println(weekday[2]);
+        System.out.println(weekday[3]);
+        System.out.println(weekday[4]);
+        System.out.println(weekday[5]);
+        System.out.println(weekday[6]);
+        
+        
+        
+        
+                
+       
+    }
+    
+}
