55ab840c93a2a6f00e116ed3516b17a57e38b6ae
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-11-11 20:42:41 +0430

    Add files via upload

---
+++ Peractice1.java
@@ -0,0 +1,43 @@
+
+package peractice.pkg1;
+
+public class Peractice1 {
+
+    private static int x;
+    
+    
+
+   
+    public static void main(String[] args, int mark) {
+        
+        if (mark >= 90 && mark <= 100)
+            System.out.println("A");
+        else if(mark >= 80 && mark < 90)
+            System.out.println("B");
+        else if(mark>=70 && mark < 80)
+            System.out.println("C");
+        else
+            System.out.println("این شاکرد ناکام است");
+   
+        }
+    
+ 
+            
+           
+            
+        
+    }
+        
+        
+       
+       
+     
+       
+    
+    
+        
+        
+        
+    }
+    
+}
