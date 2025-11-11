910dc60d7b756d555995b21df5f77a8b2de7a907
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-11-11 20:53:52 +0430

    Add files via upload

---
+++ JavaApplication4.java
@@ -0,0 +1,38 @@
+
+package javaapplication4;
+
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        int x = 88;
+        if (x > 0) {
+            System.out.println("positve");
+           
+        }
+        
+        else if (x < 0)
+            System.out.println("negative");
+        else 
+             System.out.println("Zero");
+                
+     
+                               }
+    
+    
+}
+    
+
+                    
+            
+         
+                   
+                   
+               
+               
+               
+                  
+        
+        
+    }
+    
