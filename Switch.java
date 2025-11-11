5370fbfb62d6c4d0c658ac55c62c1c7703ec85a3
sulimanfateh2025-arch <sulimanfateh2025@gmail.com>
2025-11-11 20:39:04 +0430

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,31 @@
+
+package pkgswitch;
+
+import java.util.Scanner;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a birth year");
+        int year = input. nextInt() % 12;
+        
+        switch(year){
+            case 0:
+                System.out.println("kaw");
+                break;
+            case 1:
+                System.out.println("");
+                break;
+                
+          
+            
+        }
+        
+        
+        
+        
+        
+        }
+    
+}
